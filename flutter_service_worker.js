e88a66775e0f72a8f7b1e12aafd5d7228fab7bff
sampaguitaOS <103969117+sampaguitaOS@users.noreply.github.com>
2022-05-03 19:00:28 +0000

    Flutter Web Build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,9 +28,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9017f3e1422a730419bca28e4575961a",
-"/": "9017f3e1422a730419bca28e4575961a",
-"main.dart.js": "a2613c8326cc3ee00ecdbd269543b654",
+"index.html": "d6e825eccfe54d0b25488ca430eb2307",
+"/": "d6e825eccfe54d0b25488ca430eb2307",
+"main.dart.js": "060e2ef927b9f3ee308bf2ed713026ec",
 "manifest.json": "54f0c536036c6913907b7f0a6be62654",
 "version.json": "9856ffac2adad0d6fc0db02dff02b284"
 };
